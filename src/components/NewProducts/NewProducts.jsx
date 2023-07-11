f557380a1298b3a9778d57b3e9d2132bde7f7237
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-07-11 23:16:41 +0500

    product

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -2,10 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Product from '../Product/Product'
+import { motion } from 'framer-motion'
 
 const NewProducts = () => {
     return (
-        <section className='catalog'>
+        <motion.section key="newproducts" transition={{duration: 0.5, delay: 0.5, easings: "linear"}} exit={{opacity: 0}} initial={{ opacity: 0 }} animate={{ opacity: 1 }} className='catalog'>
             <div className='catalog__body'>
                 <h2 className='catalog__title'>НОВИНКИ</h2>
                 <div className='catalog__content'>
@@ -20,8 +21,9 @@ const NewProducts = () => {
                     <Product img="product-1" title="EVIAN ONE" price="12 800₽"/>
                     <Product img="product-1" title="EVIAN ONE" price="12 800₽"/>
                 </div>
+                <Link href="/c/mebel" className='catalog__btn'>ПОСМОТРЕТЬ ВСЕ НОВИНКИ</Link>
             </div>
-        </section>
+        </motion.section>
     )
 }
  
