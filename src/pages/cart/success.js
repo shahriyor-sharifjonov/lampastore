40aa067e7da707b9623d268dba0a7e741f298e77
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-07-31 11:09:42 +0500

    init

--- src/pages/cart/success.js
+++ src/pages/cart/success.js
@@ -5,10 +5,10 @@ import { motion } from 'framer-motion'
 const Success = () => {
     const router = useRouter()
     return (
-        <div className='cart__success' key={`${router.asPath}success`} transition={{duration: 0.5, delay: 0.5, easings: "linear"}} exit={{opacity: 0}} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <motion.div className='cart__success' key={`${router.asPath}success`} transition={{duration: 0.5, delay: 0.5, easings: "linear"}} exit={{opacity: 0}} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
             <h1>Спасибо за заказ</h1>
-            <p>МЫ СВЯЖЕМСЯ С ВАМИ В САМОЕ БЛИЖАЙШЕЕ ВРЕМЯ, ЧТОБЫ УТОЧНИТЬ ДЕТАЛИ И ОФОРМИТЬ ПОКУПКУ.</p>
-        </div>
+            <p>Мы свяжемся с вами в самое ближайшее время, чтобы уточнить детали и оформить покупку.</p>
+        </motion.div>
     )
 }
 
