7a178a3e124e80905d95b59a22e964293a27478d
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-08-10 07:35:55 +0500

    init

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -4,6 +4,7 @@ import Product from '../Product/Product'
 import { motion } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { useSelector, useDispatch } from 'react-redux'
+import styles from '@/components/Top/Top.module.scss'
 
 const Catalog = ({categoryTitle, category, more, categorySlug, subcategory, subcategorySlug, filter}) => {
     const router = useRouter()
@@ -12,7 +13,9 @@ const Catalog = ({categoryTitle, category, more, categorySlug, subcategory, subc
     const maxPrice = useSelector((state) => state.filters.maxPrice)
     const filters = useSelector((state) => state.filters)
     const [newProducts, setNewProducts] = useState([])
+    const [search, setSearch] = useState('')
     const [productsByCategory, setProductsByCategory] = useState([])
+    const [productsBySearch, setProductsBySearch] = useState([])
     const [productsBySubcategory, setProductsBySubcategory] = useState([])
 
     useEffect(() => {
@@ -106,59 +109,98 @@ const Catalog = ({categoryTitle, category, more, categorySlug, subcategory, subc
         setProductsBySubcategory(finalProd)
     }, [productsByCategory, subcategory, filters])
 
-    return (
-        <motion.section key={`${router.asPath}newproducts5121`} transition={{duration: 0.5, delay: 0.5, easings: "linear"}} exit={{opacity: 0}} initial={{ opacity: 0 }} animate={{ opacity: 1 }} className='catalog'>
-            <div className='catalog__body'>
-                <>
-                    {category === 'news' && newProducts.length >= 1 && (
-                        <>
-                            {more ? (
-                                <h2 className='catalog__title'>{categoryTitle}</h2>
-                            ) : ''}
-                            <div className='catalog__content'>
-                                {
-                                newProducts.map(product => (
-                                    <Product key={product._id} id={product._id} img={product.images[0]?.url} title={product.title} price={product.price} data={product.created_at} vip={product.vip}/>
-                                ))}
-                            </div>
-                            {more ? (
-                                <Link href="/c/news" className='catalog__btn'>ПОСМОТРЕТЬ ВСЕ</Link>
-                            ) : ''}
-                        </>
-                    )}
-                    
-                    {category !== 'news' && productsByCategory.length >= 1 ? (
-                        <>
-                            {more ? (
-                                <h2 className='catalog__title'>{categoryTitle}</h2>
-                            ) : ''}
-                            <div className='catalog__content'>
-                            {
-                                !subcategory ? (productsByCategory.map(product => (
-                                    <Product key={product._id} id={product._id} img={product.images[0]?.url} title={product.title} price={product.price} data={product.created_at} vip={product.vip}/>
-                                ))) : (productsBySubcategory.map(product => (
-                                    <Product key={product._id} id={product._id} img={product.images[0]?.url} title={product.title} price={product.price} data={product.created_at} vip={product.vip}/>
-                                )))
-                            }
-                            </div>
-                            {more ? (
-                                <Link href={`/c/${categorySlug}`} className='catalog__btn'>ПОСМОТРЕТЬ ВСЕ</Link>
-                            ) : ''}
-                        </>
-                    ) : ""}
+    useEffect(() => {
+        if (search.length > 0) {
+            const filteredProducts = products.filter((product) =>
+                product.title.toLowerCase().includes(search.toLowerCase())
+            );
+    
+            setProductsBySearch(filteredProducts);
+        } else {
+            setProductsBySearch([]);
+        }
+    }, [search, products]);
+    
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    };
 
-                    {subcategory && category !== 'news' && productsBySubcategory.length === 0 ? (
-                        <h2 className='catalog__title center'>Ничего не найдено.</h2>
-                    ) : ""}
-                    
-                    {!subcategory && category !== 'news' && productsByCategory.length === 0 ? (
-                        <h2 className='catalog__title center'>Ничего не найдено.</h2>
-                    ) : ""}
-                </>
-            </div>
-        </motion.section>
-        
-    )
+    if(category !== 'search') {
+        return (
+            <motion.section key={`${router.asPath}newproducts5121`} transition={{duration: 0.5, delay: 0.5, easings: "linear"}} exit={{opacity: 0}} initial={{ opacity: 0 }} animate={{ opacity: 1 }} className='catalog'>
+                <div className='catalog__body'>
+                    <>
+                        {category === 'news' && newProducts.length >= 1 && (
+                            <>
+                                {more ? (
+                                    <h2 className='catalog__title'>{categoryTitle}</h2>
+                                ) : ''}
+                                <div className='catalog__content'>
+                                    {
+                                    newProducts.map(product => (
+                                        <Product key={product._id} id={product._id} img={product.images[0]?.url} title={product.title} price={product.price} data={product.created_at} vip={product.vip}/>
+                                    ))}
+                                </div>
+                                {more ? (
+                                    <Link href="/c/news" className='catalog__btn'>ПОСМОТРЕТЬ ВСЕ</Link>
+                                ) : ''}
+                            </>
+                        )}
+                        
+                        {category !== 'news' && productsByCategory.length >= 1 ? (
+                            <>
+                                {more ? (
+                                    <h2 className='catalog__title'>{categoryTitle}</h2>
+                                ) : ''}
+                                <div className='catalog__content'>
+                                {
+                                    !subcategory ? (productsByCategory.map(product => (
+                                        <Product key={product._id} id={product._id} img={product.images[0]?.url} title={product.title} price={product.price} data={product.created_at} vip={product.vip}/>
+                                    ))) : (productsBySubcategory.map(product => (
+                                        <Product key={product._id} id={product._id} img={product.images[0]?.url} title={product.title} price={product.price} data={product.created_at} vip={product.vip}/>
+                                    )))
+                                }
+                                </div>
+                                {more ? (
+                                    <Link href={`/c/${categorySlug}`} className='catalog__btn'>ПОСМОТРЕТЬ ВСЕ</Link>
+                                ) : ''}
+                            </>
+                        ) : ""}
+
+                        {subcategory && category !== 'news' && category !== 'search' && productsBySubcategory.length === 0 ? (
+                            <h2 className='catalog__title center'>Ничего не найдено.</h2>
+                        ) : ""}
+                        
+                        {!subcategory && category !== 'news' && category !== 'search' && productsByCategory.length === 0 ? (
+                            <h2 className='catalog__title center'>Ничего не найдено.</h2>
+                        ) : ""}
+                    </>
+                </div>
+            </motion.section>
+        )
+    } else {
+        return (
+            <>
+                <motion.section key={`${router.asPath}top`} transition={{duration: 0.5, delay: 0.5, easings: "linear"}} exit={{opacity: 0}} initial={{ opacity: 0 }} animate={{ opacity: 1 }} className={styles.top}>
+                    <h1 className={styles.title}>ПОИСК</h1>
+                    <form className={styles.search}>
+                        <input type="text" placeholder='Поиск' value={search} onInput={(e) => {handleSearch(e)}}className={styles.input}/>
+                    </form>
+                </motion.section>
+                <div className='catalog__body'>
+                    {productsBySearch.length !== 0 && <h2 className='catalog__title'>Найдено {productsBySearch.length} продуктов</h2>}
+                    <div className='catalog__content'>
+                        {productsBySearch.length !== 0 ? (
+                            productsBySearch.map(product => (
+                                <Product key={product._id} id={product._id} img={product.images[0]?.url} title={product.title} price={product.price} data={product.created_at} vip={product.vip}/>
+                            ))
+                        ) : <h2 className='catalog__title center'>Ничего не найдено.</h2>}
+                    </div>
+                </div>
+            </>
+        )
+    }
+    
 }
  
 export default Catalog
