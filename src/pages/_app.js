ccede2d11d0f09b3cdad4afece3f4e82d86f1636
Shahriyor <shahriyordeveloping@gmail.com>
2024-01-10 22:26:23 +0500

    main

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,6 +11,7 @@ import Footer from '@/components/Footer/Footer'
 import FourOhFour from './404'
 import Loader from '@/components/Loader/Loader'
 import { setLoading } from '@/store/slices/loadingSlice'
+import Script from 'next/script'
 
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
@@ -33,6 +34,23 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
             <Footer />
           </div>
         </AnimatePresence>
+        <Script id="yandex">
+          {`
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(96090585, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true,
+              ecommerce:"dataLayer"
+            });
+          `}
+        </Script>
       </SessionProvider>
     </Provider>
   );
