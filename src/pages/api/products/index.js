aa618e3be61ac1d9d58538880ec7e7e111fb3058
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2025-07-02 01:50:17 +0500

    fix: backend added sort and limit to products route

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -13,12 +13,21 @@ const getProducts = async (req, res) => {
     const client = await clientPromise
     const db = client.db()
 
-    const products = await db.collection('products').find().sort({ created_at: -1 }).toArray()
-    res.status(200).json(products)
+    const { page = 1, limit = 10 } = req.query; // Получение параметров пагинации
+    const skip = (parseInt(page) - 1) * parseInt(limit); // Вычисление пропускаемых записей
+
+    const products = await db.collection('products')
+      .find()
+      .sort({ created_at: -1 })
+      .skip(skip)
+      .limit(parseInt(limit))
+      .toArray();
+
+    res.status(200).json(products);
   }
   catch (err) {
-    return res.status(500).json({err: err.message})
+    return res.status(500).json({ err: err.message });
   }
 }
 
-export default def
+export default def;
